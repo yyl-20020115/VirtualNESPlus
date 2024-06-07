f9dda45909dd3a6ecb55a46d3c242dfd6b0febc0
Yilin Yang <yyl_20050115@hotmail.com>
2024-06-07 16:52:11 +0800

    fx

---
+++ MiniNES/MiniNES.h
@@ -0,0 +1,32 @@
+﻿
+// MiniNES.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMiniNESApp:
+// 有关此类的实现，请参阅 MiniNES.cpp
+//
+
+class CMiniNESApp : public CWinApp
+{
+public:
+	CMiniNESApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMiniNESApp theApp;

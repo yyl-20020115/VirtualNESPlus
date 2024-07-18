2aa3af0656d8055b5d26bd5d2dc496af901e84ff
Yilin Yang <yyl_20050115@hotmail.com>
2024-07-18 18:36:42 +0800

    fx

--- Mapper170.cpp
+++ Mapper170.cpp
@@ -1,10 +1,54 @@
 #include "Mapper170.h"
 
-Mapper170::Mapper170(NES* parent) : Mapper(parent) {}
+Mapper170::Mapper170(NES* parent) : Mapper(parent),reg() {}
 //////////////////////////////////////////////////////////////////////////
 // Mapper170                                                            //
 //////////////////////////////////////////////////////////////////////////
 void	Mapper170::Reset()
 {
+	reg = 0;
+	switch (PROM_16K_SIZE) {
+	default:
+	case	1:	// 16K only
+		SetPROM_16K_Bank(4, 0);
+		SetPROM_16K_Bank(6, 0);
+		break;
+	case	2:	// 32K
+		SetPROM_32K_Bank(0);
+		break;
+	}
+
 
 }
+
+bool Mapper170::ReadAddress(WORD addr)
+{
+	return false;// addr == 0x7001 || addr == 0x7777;
+}
+
+bool Mapper170::WriteAddress(WORD addr)
+{
+	return false;// addr == 0x6502 || addr == 0x7000;
+}
+
+void Mapper170::Write(WORD addr, BYTE data)
+{
+	switch (addr) {
+	case 0x6502:
+	case 0x7000:
+		reg = (data << 1) & 0x80;
+		break;
+	}
+}
+
+BYTE Mapper170::Read(WORD addr, BYTE& data)
+{
+	switch (addr) {
+	case 0x7001:
+	case 0x7777:
+		data = reg;
+		break;
+	}
+	return data;
+}
+
--- Mapper170.h
+++ Mapper170.h
@@ -10,7 +10,16 @@ public:
 	Mapper170(NES* parent);
 
 	void	Reset();
+	virtual bool    ReadAddress(WORD addr) override;
+	virtual bool    WriteAddress(WORD addr) override;
+
+	// $8000-$FFFF Memory write
+	virtual	void	Write(WORD addr, BYTE data) override;
+
+	// $8000-$FFFF Memory read(Dummy)
+	virtual	BYTE	Read(WORD addr, BYTE& data) override;
 
 protected:
+	BYTE	reg;
 private:
 };
